d6acc53b75ce3273fe290087ab399c80c8fcfd88
Opalo1189 <enriquemaestre11@gmail.com>
2023-10-12 23:54:14 -0500

    Este es el que recibe desde el canal de comunicacion pubsub

--- mf-shell/src/app/app.component.ts
+++ mf-shell/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonsLibService } from './lib/commons-lib.service';
+import PubSub from 'pubsub-js';
+import { ICommonProduct } from './lib/models/product.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,17 @@ import { CommonsLibService } from './lib/commons-lib.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(public commonsLibService: CommonsLibService){ }
+
+  private _products: ICommonProduct[] = []
+  count = 0
+
+  constructor(public commonsLibService: CommonsLibService) { }
+
+  ngOnInit(): void {
+    PubSub.subscribe('products', (_message, data) => { //Aqui no se sabe que data esta llegando porque esta en otro proyecto
+      this._products.push(data as unknown as ICommonProduct)//por eso se convierte a unknown y depues al tipo que se necesita, el equipo debe saber que estructura de datos se maneja
+      this.count++;
+      localStorage.setItem('products', JSON.stringify(this._products))
+    })
+  }
 }
